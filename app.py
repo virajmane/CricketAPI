024575002f946ac85ba29398f4c242659ac23021
Viraj <62030782+virajmane@users.noreply.github.com>
2020-10-06 15:47:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,21 @@ from flask import *
 from pycricbuzz import Cricbuzz
 import json
 
-c = Cricbuzz()
-match = c.matches()
-mid = (match[0]['id'])
-livescore = c.livescore(mid=mid)
-scorecard = c.scorecard(mid=mid)
-matchinfo = c.matchinfo(mid=mid)
-commentary = c.commentary(mid=mid)
-
-result = {"matches": match, "livescore": livescore, "scorecard": scorecard, "matchinfo": matchinfo, "commentary": commentary}
-final = json.dumps(result)
-
-app = Flask('__init__')
+app = Flask(__name__)
 @app.route("/")
 def index():
+  c = Cricbuzz()
+  match = c.matches()
+  mid = (match[0]['id'])
+  livescore = c.livescore(mid=mid)
+  scorecard = c.scorecard(mid=mid)
+  matchinfo = c.matchinfo(mid=mid)
+  commentary = c.commentary(mid=mid)
+
+  result = {"matches": match, "livescore": livescore, "scorecard": scorecard, "matchinfo": matchinfo, "commentary": commentary}
+  final = json.dumps(result)
   return f"<code>{final}</code>"
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000, use_reloader=True, threaded=True)
